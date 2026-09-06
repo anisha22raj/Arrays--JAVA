7e67c73fe24221822613fa9a2607ba91a3040a14
Anisha Raj <anisha.raj2206@gmail.com>
2026-09-06 23:58:05 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,18 @@
+class RemoveDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        int j = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
